9e5f998e4f02eb3eaa87e8da8e1c03d99175086d
frazbakht5 <mr.fraz.bakht@gmail.com>
2023-01-19 02:17:09 +0500

    kept only required function

--- hackerrank/Breaking the Records.js
+++ hackerrank/Breaking the Records.js
@@ -1,27 +1,5 @@
 'use strict';
 
-const fs = require('fs');
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', function (inputStdin) {
-	inputString += inputStdin;
-});
-
-process.stdin.on('end', function () {
-	inputString = inputString.split('\n');
-
-	main();
-});
-
-function readLine() {
-	return inputString[currentLine++];
-}
-
 /*
  * Complete the 'breakingRecords' function below.
  *
@@ -55,18 +33,4 @@ function breakingRecords(scores) {
 
 	return [maxCount, minCount];
 
-}
-
-function main() {
-	const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-	const n = parseInt(readLine().trim(), 10);
-
-	const scores = readLine().replace(/\s+$/g, '').split(' ').map(scoresTemp => parseInt(scoresTemp, 10));
-
-	const result = breakingRecords(scores);
-
-	ws.write(result.join(' ') + '\n');
-
-	ws.end();
 }
