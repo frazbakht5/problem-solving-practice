c467b75d30ba1cfe323675d0a989843d78c5bfdd
frazbakht5 <mr.fraz.bakht@gmail.com>
2023-01-19 05:02:19 +0500

    incomplete

--- hackerrank/Subarray Division.js
+++ hackerrank/Subarray Division.js
@@ -1,26 +1,3 @@
-'use strict';
-
-const fs = require('fs');
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', function (inputStdin) {
-	inputString += inputStdin;
-});
-
-process.stdin.on('end', function () {
-	inputString = inputString.split('\n');
-
-	main();
-});
-
-function readLine() {
-	return inputString[currentLine++];
-}
 
 /*
  * Complete the 'birthday' function below.
@@ -35,24 +12,89 @@ function readLine() {
 function birthday(s, d, m) {
 	// Write your code here
 
+	let allCombinations = getCombos(s, m);
+	let count = 0;
+
+	let unallowedPermutationIndexes = [];
+	let n = s.length;
+
+	if (n > 1) {
+		for (let i = 0; i < allCombinations.length; i++) {
+			let subArrayNumber = parseInt(i / n);
+			// console.log("For i = " + i + ", subArrayNumber ===> ", subArrayNumber);
+
+			// unallowedPermutationIndexes.push()
+			for (let j = 0; j <= subArrayNumber; j++) {
+				// console.log("pushing i = ", i);
+				unallowedPermutationIndexes.push(subArrayNumber * n + j)
+
+			}
+			i += subArrayNumber;
+			// unallowedPermutationIndexes.push(i);
+		}
+	}
+	unallowedPermutationIndexes = [...new Set(unallowedPermutationIndexes)];
+	console.log("unallowedPermutationIndexes ===> ", unallowedPermutationIndexes);
+
+	let yesCombinations = [];
+
+	for (let i = 0; i < allCombinations.length; i++) {
+		const combination = allCombinations[i];
+		console.log("For i = " + i + ", combination ===> ", combination);
+
+		if (unallowedPermutationIndexes.includes(i)) {
+			console.log("Skipping");
+			continue;
+		}
+
+
+		let sum = 0;
+
+		for (let j = 0; j < combination.length; j++) {
+			const num = combination[j];
+			sum += num;
+
+			if (sum > d)
+				break;
+		}
+		if (sum == d && !yesCombinations.includes(JSON.stringify(combination))) {
+			yesCombinations.push(JSON.stringify(combination));
+			count++;
+			console.log("Yes");
+		}
+		else {
+			console.log("No");
+		}
+
+	}
+	return count;
+
 }
 
-function main() {
-	const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+const getCombos = (arr, len) => {
+	const base = arr.length
+	const counter = Array(len).fill(base === 1 ? arr[0] : 0)
+	if (base === 1) return [counter]
+	const combos = []
+	const increment = i => {
+		if (counter[i] === base - 1) {
+			counter[i] = 0
+			increment(i - 1)
+		} else {
+			counter[i]++
+		}
+	}
 
-	const n = parseInt(readLine().trim(), 10);
+	for (let i = base ** len; i--;) {
+		const combo = []
+		for (let j = 0; j < counter.length; j++) {
+			combo.push(arr[counter[j]])
+		}
+		combos.push(combo)
+		increment(counter.length - 1)
+	}
 
-	const s = readLine().replace(/\s+$/g, '').split(' ').map(sTemp => parseInt(sTemp, 10));
-
-	const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
-
-	const d = parseInt(firstMultipleInput[0], 10);
-
-	const m = parseInt(firstMultipleInput[1], 10);
-
-	const result = birthday(s, d, m);
-
-	ws.write(result + '\n');
-
-	ws.end();
+	return combos
 }
+
+console.log(birthday([1, 2, 1, 3, 2], 3, 2));
