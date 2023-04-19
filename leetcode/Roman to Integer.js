a6c82094fd230912e30fc1ef2e6b32de984b1e56
frazbakht5 <mr.fraz.bakht@gmail.com>
2023-04-19 12:05:08 +0500

    new problem

---
+++ leetcode/Roman to Integer.js
@@ -0,0 +1,18 @@
+const symbolValues = {
+	"I": 1,
+	"V": 5,
+	"X": 10,
+	"L": 50,
+	"C": 100,
+	"D": 500,
+	"M": 1000
+}
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+
+};
