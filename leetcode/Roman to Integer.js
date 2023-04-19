f6daf39344cd1ece2c6bfbcb421e11d71402b0a2
frazbakht5 <mr.fraz.bakht@gmail.com>
2023-04-19 12:12:25 +0500

    first attempt

--- leetcode/Roman to Integer.js
+++ leetcode/Roman to Integer.js
@@ -15,4 +15,20 @@ const symbolValues = {
  */
 var romanToInt = function (s) {
 
+	if (s.length == 0)
+		return 0;
+
+	let total = 0;
+	let lastChar = s[0];
+
+	for (let i = 0; i < s.length; i++) {
+		let char = s[i];
+
+		if (symbolValues[char] <= symbolValues[lastChar])
+			total += symbolValues[char];
+		else
+			total -= symbolValues[char];
+	}
+
+	return total;
 };
